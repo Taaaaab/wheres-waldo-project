8f8800b42b35a492a4d42db87d311e47fba03d5a
Taaaaab <owen.whitsett@gmail.com>
2022-11-01 12:29:56 -0400

    removed error message & updated stylesheet

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 
 let setCurrentChar = '';
 
-const errorMsg = document.querySelector('.errorMsg');
-
-const homer = document.querySelector('.homerImg');
-homer.addEventListener('click', () => {
-    errorMsg.innerHTML = "";
-    homer.classList.add('selectBorder');
-    setCurrentChar = 'homer'
-});
-
 const wallpaperBox = document.querySelector('.wallpaperBox');
 
 const wallpaper = document.querySelector('.wallpaper');
 wallpaper.addEventListener('click', (e) => {
-    if(setCurrentChar === '') {
-        errorMsg.innerHTML = "Select a character first!"
-    } else {
-        const box = document.createElement('div');
-        box.classList.add('targetBox');
-        box.style.left = (e.pageX - 32 + 'px');
-        box.style.top = (e.pageY - 272 + 'px');
-        wallpaperBox.append(box);
-        console.log(e.clientX);
+    if (document.querySelector('.targetBox')) {
+        document.querySelector('.targetBox').remove();
     }
+    const box = document.createElement('div');
+    box.classList.add('targetBox');
+    box.style.left = (e.pageX - 32 + 'px');
+    box.style.top = (e.pageY - 272 + 'px');
+    wallpaperBox.append(box);
+    
 });
