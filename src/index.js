1f6f3391b9e9d79a6cef020b3b99602f941e453e
Taaaaab <owen.whitsett@gmail.com>
2022-11-07 15:40:02 -0500

    added firebase & firestore database integration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
 
-let setCurrentChar = '';
+const firebaseConfig = {
+    apiKey: "AIzaSyCeAPLqSmgf1RwkukFhUyqCA9gdDLofmeM",
+    authDomain: "wheres-waldo-64ad0.firebaseapp.com",
+    projectId: "wheres-waldo-64ad0",
+    storageBucket: "wheres-waldo-64ad0.appspot.com",
+    messagingSenderId: "669960801547",
+    appId: "1:669960801547:web:5cd32ea4cbe8b73b0a660e"
+  };
+
+// initialize firebase app
+initializeApp(firebaseConfig);
+// init firebase services
+const db = getFirestore();
+// collection ref
+const colRef = collection(db, 'char-locations');
+// get collection data
+let charLocationArray = [];
+
+getDocs(colRef)
+  .then((snapshot) => {
+    let myArray = []
+    snapshot.docs.forEach((doc) => {
+        myArray.push({ ...doc.data(), id: doc.id })
+    })
+    return charLocationArray.push(myArray);
+  })
+  .catch(err => {
+    console.log(err.message);
+});
+
+let userClickArray = [];
 
 const wallpaperBox = document.querySelector('.wallpaperBox');
 
@@ -9,12 +41,40 @@ wallpaper.addEventListener('click', (e) => {
         document.querySelector('.targetBox').remove();
     }
     const box = document.createElement('div');
-    let x = e.clientX - 20;
-    let y = e.clientY - 30;
-    box.style.left = `${x}px`;
-    box.style.top = `${y}px`;
+    let x = e.clientX;
+    let y = e.clientY;
+    box.style.left = `${x - e.target.offsetLeft - 60}px`;
+    box.style.top = `${y - e.target.offsetTop - 180}px`;
     box.classList.add('targetBox');
     box.innerHTML = 'Homer';
     wallpaperBox.append(box);
     
+    storeUserClick(e);
+    matchUserToDatabase(userClickArray, charLocationArray);
 });
+
+function storeUserClick(e) {
+    userClickArray = [];
+    let x = e.clientX;
+    let y = e.clientY;
+    let xPos = (x - e.target.offsetLeft);
+    let yPos = (y - e.target.offsetTop);
+  
+    userClickArray = [xPos, yPos];
+    
+    return userClickArray;
+};
+
+function matchUserToDatabase(userArray, fireArray) {
+    let userPosX = userArray[0];
+    let dbPosX = fireArray[0][0].Array;
+    console.log(userPosX);
+    console.log(dbPosX);
+    if (dbPosX.includes(userPosX)) {
+        console.log('Match Found!');
+        return 
+    } else {
+        console.log('No match');
+        return 
+    }
+};
