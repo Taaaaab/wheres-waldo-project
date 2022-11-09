aa4dfc9ea6d3d06f9e3aca270ac547e1a15e38ff
Taaaaab <owen.whitsett@gmail.com>
2022-11-09 08:28:43 -0500

    working timer which stops upon finding all 3 characters

--- src/index.js
+++ src/index.js
@@ -181,3 +181,61 @@ function matchUserToBart(userArray, fireArray) {
         return 
     }
 };
+
+const timerTitle = document.getElementById('timerTitle');
+const timer = document.getElementById('timer');
+let timerInterval;
+
+window.onload = function() {
+    startTimer();
+
+};
+
+function startTimer() {
+    // First we start by clearing the existing timer, in case of a restart
+    clearInterval(timerInterval);
+    // Then we clear the variables
+    let second = 0,
+      minute = 0,
+      hour = 0;
+    
+    // Next we set a interval every 1000 ms
+    timerInterval = setInterval(function () {
+    
+    // Check to see if paused
+    stopTimer();
+    // Next, we add a new second since one second is passed
+    second++;
+    
+    // We set the timer text to include a two digit representation
+      timer.innerHTML =
+        (hour ? hour + ':' : '') +
+        (minute < 10 ? '0' + minute : minute) +
+        ':' +
+        (second < 10 ? '0' + second : second);
+        
+      // We check if the second equals 60 "one minute"
+      if (second == 60) {
+        // If so, we add a minute and reset our seconds to 0
+        minute++;
+        second = 0;
+      }
+  
+      // If we hit 60 minutes "one hour" we reset the minutes and plus an hour
+      if (minute == 60) {
+        hour++;
+        minute = 0;
+      }
+    }, 1000);
+    
+};
+
+function stopTimer() {
+    if (document.querySelector('.characterHomer').innerHTML === 'Homer Found!' &&
+    document.querySelector('.characterApu').innerHTML === 'Apu Found!' &&
+    document.querySelector('.characterBart').innerHTML === 'Bart Found!') {
+        clearInterval(timerInterval);
+        timerInterval = undefined;
+        timerTitle.innerHTML = 'Time to find all 3 characters:';
+    }
+}
